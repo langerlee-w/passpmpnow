8bc12c892d6cf28d09810e6a1b17b175da10888a
langerlee-w <langer.lee@gmail.com>
2025-11-08 09:03:57 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,6 +26,7 @@ export default function Footer() {
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
           <Link href="/privacy">Privacy</Link>
+          <Link href="/contact">Contact</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
